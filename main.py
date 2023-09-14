76d3a189a2455f5a61d80f5c5d9c74855b82e713
evgeny <vorobyev.ev@gmail.com>
2023-09-15 02:59:59 +0300

    Function to create a new note

--- main.py
+++ main.py
@@ -9,4 +9,20 @@ def load_notes():
         return notes_data
     return []
 
+def save_notes(notes):
+    with open('notes.json', 'w') as file:
+        json.dump(notes, file, indent=4)
 
+def create_note():
+    title = input("Enter the title of the note: ")
+    body = input("Enter the body of the note: ")
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    note = {
+        "id": len(notes) + 1,
+        "title": title,
+        "body": body,
+        "timestamp": timestamp
+    }
+    notes.append(note)
+    save_notes(notes)
+    print("Note successfully saved!")
