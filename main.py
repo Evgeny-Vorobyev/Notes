ed6fa9766b9b7d14dd0579c68b1c790b89a3a7c7
evgeny <vorobyev.ev@gmail.com>
2023-09-15 01:27:06 +0300

    Function to load notes from a json file

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 
+import json
+import os
+from datetime import datetime
+def load_notes():
+    if os.path.exists('notes.json'):
+        with open('notes.json', 'r') as file:
+            notes_data = json.load(file)
+        return notes_data
+    return []
 
 
