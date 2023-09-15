18bb1acae822e070db01357405d65fd312cde5cd
evgeny <vorobyev.ev@gmail.com>
2023-09-15 09:17:22 +0300

    Main program loop

--- main.py
+++ main.py
@@ -9,9 +9,13 @@ def load_notes():
         return notes_data
     return []
 
+
+
 def save_notes(notes):
     with open('notes.json', 'w') as file:
         json.dump(notes, file, indent=4)
+
+
 
 def create_note():
     title = input("Enter the title of the note: ")
@@ -27,6 +31,8 @@ def create_note():
     save_notes(notes)
     print("Note successfully saved!")
 
+
+
 def list_notes():
     for note in notes:
         print(f"ID: {note['id']}")
@@ -34,6 +40,8 @@ def list_notes():
         print(f"Body: {note['body']}")
         print(f"Timestamp: {note['timestamp']}")
         print()
+
+
 
 def edit_note():
     note_id = int(input("Enter the ID of the note you want to edit: "))
@@ -49,6 +57,8 @@ def edit_note():
             return
     print("Note not found.")
 
+
+
 def delete_note():
     note_id = int(input("Enter the ID of the note you want to delete: "))
     for note in notes:
@@ -58,6 +68,33 @@ def delete_note():
             print("Note successfully deleted!")
             return
     print("Note not found.")
+
+
+
+notes = load_notes()
+while True:
+    print("\nNote Management System")
+    print("1. Add a new note")
+    print("2. List all notes")
+    print("3. Edit a note")
+    print("4. Delete a note")
+    print("5. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == '1':
+        create_note()
+    elif choice == '2':
+        list_notes()
+    elif choice == '3':
+        edit_note()
+    elif choice == '4':
+        delete_note()
+    elif choice == '5':
+        print("Exiting...")
+        break
+    else:
+        print("Invalid choice. Please try again.")
 
 
 
